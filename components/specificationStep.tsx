2a7746342a8a0c738c9bf376b93648249af0c688
tedvanwijk <tedvanwijk@gmail.com>
2024-07-23 19:21:53 +0200

    Fixed chevron being in the wrong orientation if defaultChecked enabled

--- components/specificationStep.tsx
+++ components/specificationStep.tsx
@@ -1,5 +1,5 @@
 import { ChevronDown, ChevronUp } from "react-feather";
-import { useRef } from "react";
+import { useState } from "react";
 
 export default function SpecificationStep(
     {
@@ -21,10 +21,10 @@ export default function SpecificationStep(
             arrowEnabled: boolean
         }
 ) {
-    const checkbox: any = useRef(null);
+    const [open, setOpen] = useState(defaultChecked);
     return (
         <div className={`collapse ${enabled ? '' : 'opacity-50'} ${forceOpen ? 'collapse-open' : ''} bg-base-200 mb-3`}>
-            <input defaultChecked={defaultChecked} type="checkbox" className={enabled ? '' : '!cursor-default'} disabled={!enabled} ref={checkbox} />
+            <input checked={open} onChange={e => setOpen(e.target.checked)} type="checkbox" className={enabled ? '' : '!cursor-default'} disabled={!enabled} />
             <div className="collapse-title text-xl font-bold flex flex-row justify-between w-full items-center">
                 <div className="flex flex-row justify-start items-center">
                     <div className={`mr-3 bg-base-300 rounded-full relative h-10 w-10 text-center align-middle leading-10`}>
@@ -34,7 +34,7 @@ export default function SpecificationStep(
                     {header}
                 </div>
                 <div className="aspect-square h-[2.5rem]">
-                    {arrowEnabled ? (checkbox.current?.checked ? <ChevronUp className="text-xl w-full h-full" /> : <ChevronDown className="text-xl w-full h-full" />) : ("")}
+                    {arrowEnabled ? (open ? <ChevronUp className="text-xl w-full h-full" /> : <ChevronDown className="text-xl w-full h-full" />) : ("")}
                 </div>
             </div>
 
