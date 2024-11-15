d00c51eb99219d93d8fe9b68f94fcde882a39601
tedvanwijk <tedvanwijk@gmail.com>
2024-11-15 19:55:59 +0100

    Added status display

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
 import { Version } from './types';
 
 export default async function Home() {
+  const status = await fetch(
+    `${process.env.API_URL}/status`,
+    {
+      method: "GET",
+      cache: "no-cache"
+    }
+  ).then(res => res.status);
+
+  let statusElement;
+  switch (status) {
+    case 204:
+      statusElement = <div className='badge p-4 bg-green-700 text-base-100'>Running</div>
+      break;
+    case 503:
+      statusElement = <div className='badge p-4 bg-accent'>Maintenance</div>
+      break;
+    default:
+      statusElement = <div className='badge p-4 bg-red-700 text-base-100'>Not running</div>
+  }
+
   const versions = await fetch(
     `${process.env.API_URL}/versions`,
     {
@@ -10,7 +30,13 @@ export default async function Home() {
   ).then(res => res.json());
   return (
     <div className="flex flex-col justify-start items-start h-full">
-      <h1 className='text-xl font-bold'>Home</h1>
+      <div className='flex flex-row justify-between items-center w-full'>
+        <h1 className='text-xl font-bold'>Home</h1>
+        <div className='flex flex-row justify-end items-center'>
+          <h1 className='text mr-2'>Server status:</h1>
+          {statusElement}
+        </div>
+      </div>
       <h1 className='my-2'>Version History</h1>
       <div className='w-full bg-secondary bg-opacity-50 rounded-md p-2 h-full overflow-auto flex flex-col'>
         {
