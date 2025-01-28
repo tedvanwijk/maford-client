a19fae938a27a3f37165ae5ac68e0f3ab034eedc
tedvanwijk <tedvanwijk@gmail.com>
2025-01-28 17:49:30 +0100

    Added tolerance table on drawing toggle and refactored

--- components/specifications/edit/customDrawingTypeInput.tsx
+++ components/specifications/edit/customDrawingTypeInput.tsx
@@ -2,26 +2,23 @@ import { useRef, useState } from "react";
 import { useFormContext } from 'react-hook-form';
 import { apiUrl } from "@/lib/api";
 
-export default function CustomDrawingTypeInput({
-
-}: {
-
-    }) {
-    const { register, getValues, setValue } = useFormContext();
-
+export default function CustomDrawingTypeInput() {
+    const { register, getValues, setValue, watch } = useFormContext();
     const inputFileElement = useRef(null);
-    const [label, setLabel] = useState('Custom');
     const [oldDrawingType, setOldDrawingType] = useState('Manufacturing');
 
-    document.addEventListener('cancel', () => setValue('Prp.DrawingType', oldDrawingType));
+    document.addEventListener('cancel', () => {
+        if (getValues('Prp.DrawingTypeFilename') !== undefined) return;
+        setValue('Prp.DrawingType', 'Manufacturing');
+        setValue('Prp.DrawingTypeFilename', undefined);
+    });
 
     async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.files) {
-            setLabel('Custom format selected');
             setValue('Prp.DrawingTypeFilename', e.target.files[0].name);
 
             const formData = new FormData();
-            formData.append('file', e.target.files[0])
+            formData.append('file', e.target.files[0]);
             await fetch(
                 `/api/drawingtype`,
                 {
@@ -32,25 +29,39 @@ export default function CustomDrawingTypeInput({
             )
         } else {
             setValue('Prp.DrawingType', oldDrawingType);
+            setValue('Prp.DrawingTypeFilename', undefined);
         }
+        (inputFileElement as any).current.value = null;
     }
 
     return (
-        <div
-            className="flex flex-row justify-start align-center pb-2" key="Custom">
-            <input
-                {...register('Prp.DrawingType')}
-                type='radio'
-                value="Custom"
-                className="radio checked:bg-primary bg-base-100"
-                defaultChecked={false}
-                onClick={() => {
-                    setOldDrawingType(getValues('Prp.DrawingType'));
-                    (inputFileElement.current as any).click();
-                }}
-            />
-            <label className="ml-2" htmlFor="Custom">{getValues('Prp.DrawingTypeFilename') || 'Custom'}</label>
-            <input type="file" id="file" name="file" accept=".slddrt" ref={inputFileElement} className='hidden' onChange={handleFileChange} />
-        </div>
+        <>
+            <div
+                className="flex flex-row justify-start align-center pb-2" key="Custom">
+                <input
+                    {...register('Prp.DrawingType')}
+                    type='radio'
+                    value="Custom"
+                    className="radio checked:bg-primary bg-base-100"
+                    defaultChecked={false}
+                    onClick={() => {
+                        setOldDrawingType(getValues('Prp.DrawingType'));
+                        (inputFileElement.current as any).click();
+                    }}
+                />
+                <label className="ml-2" htmlFor="Custom">{watch('Prp.DrawingType') === 'Custom' ? watch('Prp.DrawingTypeFilename') : 'Custom'}</label>
+                <input type="file" id="file" name="file" accept=".slddrt" ref={inputFileElement} className='hidden' onChange={handleFileChange} />
+            </div>
+            <label className={`form-control w-[200px] transition-opacity flex flex-col ${watch('Prp.DrawingType') !== 'Custom' ? 'hidden' : ''}`}>
+                <div className="label">
+                    <span>Tolerance table on drawing</span>
+                </div>
+                <input
+                    {...register('Prp.TableOnDrawing', { disabled: watch('Prp.DrawingType') !== 'Custom' })}
+                    type="checkbox"
+                    className="toggle toggle-primary my-auto bg-base-300"
+                />
+            </label>
+        </>
     )
 }
