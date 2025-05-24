8cc2ec388010f36b2536f74acab81a823f2ae440
Ted van Wijk <tedvanwijk@gmail.com>
2025-05-24 15:12:49 +0200

    Updated changelog

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,14 @@ export default async function Home() {
       <h1 className='my-2'>Version History</h1>
       <div className='w-full bg-secondary bg-opacity-50 rounded-md p-2 h-full overflow-auto flex flex-col'>
         <div className="mb-4">
+          <span className='font-bold'>[0.5.1]</span> <br />
+          <span className="whitespace-pre-wrap">
+            5-24-2025<br />
+            - Fixed incorrect controller version causing spec errors<br />
+            - Fixed 'go to error' not working for fluting section
+          </span>
+        </div>
+        <div className="mb-4">
           <span className='font-bold'>[0.5.0]</span> <br />
           <span className="whitespace-pre-wrap">
             5-20-2025<br />
